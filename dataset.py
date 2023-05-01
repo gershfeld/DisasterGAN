349c38e819e86bd2f405859b4c9d2154a9d6b3db
gershfeld <53533428+gershfeld@users.noreply.github.com>
2023-05-01 18:58:27 +0300

    final chagnes

--- dataset.py
+++ dataset.py
@@ -7,23 +7,23 @@ from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
 
-disaster_type_dict = {"volcano": 0, "flooding": 1, "fire": 2}
+disaster_type_dict = {"volcano": 0, "flooding": 1, "fire": 2, "wildfire": 2, "tsunami": 3, "earthquake": 4,
+                      "hurricane": 5}
+
 
 class DisasterDataset(Dataset):
     def __init__(self, root_dir, transforms=None):
         self.transform = transforms
         self.root_dir = root_dir
         self.image_files = [f for f in os.listdir(root_dir) if not f.startswith('.')]
-        # self.pre_dir = os.path.join(root, 'pre')
-        # self.post_dir = os.path.join(root, 'post')
-        # self.samples = os.listdir(self.pre_dir)
 
     def __len__(self):
         return len(self.image_files)
 
     def __getitem__(self, index):
+        # extract the images name for finding the post and pre images
         filename = self.image_files[index]
-        location, name = filename.split('-')
+        location, name = filename.rsplit('-', 1)
         disaster_type, id, post_or_pre, _ = name.split('_')
 
         pre_dis_file = os.path.join(self.root_dir, f"{location}-{disaster_type}_{id}_pre_disaster.png")
@@ -35,5 +35,7 @@ class DisasterDataset(Dataset):
         if self.transform:
             pre_img = self.transform(pre_img)
             post_img = self.transform(post_img)
-
+        # make sure files exists and disaster type exists
+        if location in disaster_type_dict.keys():
+            location, disaster_type = disaster_type, location
         return pre_img, post_img, disaster_type_dict[disaster_type]
