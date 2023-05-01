628e90bf3d49924345057af76d130e9f95d8fad6
gershfeld <53533428+gershfeld@users.noreply.github.com>
2023-05-01 19:02:02 +0300

    final commit + resuming training

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import numpy as np
 from PIL import Image
 import time
 import datetime
-import sys
-import torch.nn as nn
-import torch.nn.functional as F
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
@@ -15,20 +12,31 @@ from torchvision.utils import save_image
 from dataset import DisasterDataset
 from model.cGAN import Discriminator, GeneratorUNet, weights_init_normal
 
+cuda = True if torch.cuda.is_available() else False
+
 img_height, img_width = 1024, 1024
 lr = 0.0002
 b1 = 0.5
 b2 = 0.999
 epoch = 0
 n_epochs = 200
-batch_size = 1
+batch_size = 4
 n_cpu = 8
-sample_interval = 500
-checkpoint_interval = 500
+sample_interval = 400
+checkpoint_interval = 10
 latent_dim = 100
 
 generator = GeneratorUNet()
 discriminator = Discriminator()
+criterion_GAN = torch.nn.MSELoss()
+criterion_pixelwise = torch.nn.L1Loss()
+
+# for resuming training
+checkpoint_G = torch.load("saved_models/generator_latest.pth", map_location=torch.device('cpu'))
+checkpoint_D = torch.load("saved_models/discriminator_latest.pth", map_location=torch.device('cpu'))
+
+generator.load_state_dict(checkpoint_G['model_state_dict'])
+discriminator.load_state_dict(checkpoint_D['model_state_dict'])
 
 generator.apply(weights_init_normal)
 discriminator.apply(weights_init_normal)
@@ -36,11 +44,19 @@ discriminator.apply(weights_init_normal)
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=lr, betas=(b1, b2))
 optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(b1, b2))
 
+optimizer_G.load_state_dict(checkpoint_G['optimizer_state_dict'])
+optimizer_D.load_state_dict(checkpoint_D['optimizer_state_dict'])
+
 transforms = transforms.Compose([
     transforms.Resize((img_height, img_width), Image.BICUBIC),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
+
+val_dataloader = DataLoader(
+    DisasterDataset(r"dataset", transforms=transforms),
+    shuffle=True,
+)
 
 dataloader = DataLoader(
     DisasterDataset(r"dataset", transforms=transforms),
@@ -52,34 +68,38 @@ Tensor = torch.FloatTensor
 
 
 def sample_images(batches_done):
-    #TODO: change the dataloader here not to work on batch
+    # TODO: change the dataloader here not to work on batch
     """Saves a generated sample from the validation set"""
-    imgs = next(iter(dataloader))
+    imgs = next(iter(val_dataloader))
     real_A = Variable(imgs[0].type(Tensor))
     real_B = Variable(imgs[1].type(Tensor))
-    disaster = torch.randint(0, 3, (1,))
+    Variable(LongTensor(np.random.randint(0, 6, batch_size)))
+    disaster = Variable(LongTensor(np.random.randint(0, 6, 1)))
     fake_B = generator(real_A, disaster)
     img_sample = torch.cat((real_A.data, fake_B.data, real_B.data), -2)
-    save_image((img_sample * 0.5) + 0.5, "images/%s.png" % batches_done, nrow=5, normalize=True)
+    save_image((img_sample * 0.5) + 0.5, "samples/%s.png" % batches_done, nrow=5, normalize=True)
 
 
-criterion_GAN = torch.nn.MSELoss()
-criterion_pixelwise = torch.nn.L1Loss()
 lambda_pixel = 100
 patch = (1, img_height // 2 ** 4, img_width // 2 ** 4)
+
+FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
 def main():
     prev_time = time.time()
-
     for e in range(n_epochs):
         for i, (pre_images, post_images, label) in enumerate(dataloader):
 
-            valid = Variable(Tensor(np.ones((pre_images.size(0), *patch))), requires_grad=False)
-            fake = Variable(Tensor(np.ones((post_images.size(0), *patch))), requires_grad=False)
+            valid = Variable(FloatTensor(np.ones((pre_images.size(0), *patch))), requires_grad=False)
+            fake = Variable(FloatTensor(np.ones((post_images.size(0), *patch))), requires_grad=False)
 
+            pre_images = Variable(pre_images.type(FloatTensor))
+            post_images = Variable(post_images.type(FloatTensor))
+            label = Variable(label.type(LongTensor))
             optimizer_G.zero_grad()
-            gen_labels = Variable(torch.LongTensor(np.random.randint(0, 3, batch_size)))
+            gen_labels = Variable(LongTensor(np.random.randint(0, 6, pre_images.shape[0])))
             fake_B = generator(pre_images, gen_labels)
             pred_fake = discriminator(fake_B, pre_images, label)
             loss_GAN = criterion_GAN(pred_fake, valid)
@@ -118,22 +138,19 @@ def main():
             time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time))
             prev_time = time.time()
 
-            # Print log
-            sys.stdout.write(
-                "\r[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f, pixel: %f, adv: %f] ETA: %s"
-                % (
-                    e,
-                    n_epochs,
-                    i,
-                    len(dataloader),
-                    loss_D.item(),
-                    loss_G.item(),
-                    loss_pixel.item(),
-                    loss_GAN.item(),
-                    time_left,
-                )
-            )
-
+            # Logging
+            print("\r[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f, pixel: %f, adv: %f] ETA: %s"
+                  % (
+                      e,
+                      n_epochs,
+                      i,
+                      len(dataloader),
+                      loss_D.item(),
+                      loss_G.item(),
+                      loss_pixel.item(),
+                      loss_GAN.item(),
+                      time_left,
+                  ))
             # If at sample interval save image
             if batches_done % sample_interval == 0:
                 sample_images(batches_done)
@@ -142,7 +159,6 @@ def main():
             # Save model checkpoints
             torch.save(generator.state_dict(), "saved_models/generator_%d.pth" % e)
             torch.save(discriminator.state_dict(), "saved_models/discriminator_%d.pth" % e)
-
 
 if __name__ == "__main__":
     main()
